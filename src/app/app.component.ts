de7f3b4c5e2a34eaf20b121604412cf6161c73a6
sushilsree98 <sushil.sree98@gmail.com>
2020-06-01 10:56:05 +0530

    user data and form group added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,37 @@ export class AppComponent {
   title = 'forms';
   defaultQuestion = "pet"
   answer = '';
+  genders=['Male', 'Female'];
+  submitted = false;
+  user = {
+    username:'',
+    email:'',
+    secret:'',
+    answer:'',
+    reply:'',
+    gender:'',
+  }
   @ViewChild('f')forms:NgForm
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.forms.form.setValue({}) Overwrites the existing data in the form
+
+    //patch value updates the existing field alone
+    this.forms.form.patchValue({
+      userData:{
+        username:suggestedName
+      }
+    })
   }
 
   onSubmit(){
+    this.user.username = this.forms.value.userData.username;
+    this.user.email = this.forms.value.userData.email;
+    this.user.secret = this.forms.value.secret;
+    this.user.answer = this.forms.value.reply;
+    this.user.gender = this.forms.value.gender;
+    this.submitted = true;
     console.log(this.forms);
+    this.forms.reset();
   }
 }
