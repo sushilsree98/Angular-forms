55c25744c206ee5d540eba9bbdc1c8b20c94cdb3
sushilsree98 <sushil.sree98@gmail.com>
2020-06-03 10:38:01 +0530

    reactive forms added

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  myForms :FormGroup;
+
+  ngOnInit(){
+    this.myForms = new FormGroup({
+      'username': new FormControl(null,Validators.required),
+      'email': new FormControl(null,[Validators.required,Validators.email]),
+      'gender': new  FormControl('male',Validators.required)
+    })
+  }
+
+  onSubmit(){
+    console.log(this.myForms.value);
+  }
+}
