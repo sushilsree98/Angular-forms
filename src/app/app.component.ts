370f6b195b6c32ac2d9525aa1744fb52419f909d
sushilsree98 <sushil.sree98@gmail.com>
2020-06-05 10:25:36 +0530

    dynamic forms added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +12,25 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){
     this.myForms = new FormGroup({
-      'username': new FormControl(null,Validators.required),
-      'email': new FormControl(null,[Validators.required,Validators.email]),
-      'gender': new  FormControl('male',Validators.required)
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new  FormControl('male',Validators.required),
+      'hobbies': new FormArray([])
     })
   }
 
   onSubmit(){
     console.log(this.myForms.value);
+  }
+
+  addHobby(){
+    const controls = new FormControl(null,Validators.required);
+    (<FormArray>this.myForms.get('hobbies')).push(controls)
+  }
+
+  getControls(){
+    return (<FormArray>this.myForms.get('hobbies')).controls;
   }
 }
