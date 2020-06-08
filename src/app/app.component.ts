512c38708d8d5b8efe5b62c19a0b1b101a53df45
sushilsree98 <sushil.sree98@gmail.com>
2020-06-08 10:48:50 +0530

    Async Validators and other methods added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -14,15 +15,39 @@ export class AppComponent implements OnInit {
     this.myForms = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required,this.checkValidity.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email],this.checkAsync),
       }),
       'gender': new  FormControl('male',Validators.required),
       'hobbies': new FormArray([])
+    })
+    // this.myForms.valueChanges
+    //   .subscribe(value=>{
+    //     console.log(value);
+    //   })
+
+    // this.myForms.statusChanges
+    //   .subscribe(status=>{
+    //     console.log(status);
+    //   })
+    
+    // this.myForms.setValue({
+    //   'userData':{
+    //     'username':"Sushil",
+    //     'email':'test@test.com'
+    //   },
+    //   'gender':'male',
+    //   'hobbies':'coding'
+    // })
+    this.myForms.patchValue({
+      'userData':{
+        'username':'Sushil'
+      }
     })
   }
 
   onSubmit(){
     console.log(this.myForms);
+    this.myForms.reset();
   }
 
   addHobby(){
@@ -38,8 +63,20 @@ export class AppComponent implements OnInit {
     if(this.invalidUsernames.indexOf(control.value)!==-1){
       return {'nameIsForbidden':true};
     }else{
-      return null;
+      return;
     }
   }
 
+  checkAsync(control:FormControl):Promise<any>{
+    const promise = new Promise<any>((resolve,reject)=>{
+      setTimeout(()=>{
+        if(control.value === "test@test.com"){
+          resolve({'emailIsForbidden':true})
+        }else{
+          resolve(null);
+        }
+      },1500);
+    });
+      return promise;
+  }
 }
